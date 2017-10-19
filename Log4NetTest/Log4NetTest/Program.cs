2a1d52c095f7d4d6be74fd7ceaf1172b6e382856
forrestcai <caizi-wg@163.com>
2017-10-19 23:27:50 +0800

    t

---
+++ Log4NetTest/Log4NetTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using log4net;
+using System;
+
+namespace Log4NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger("Log");
+            logger.Error("1234");
+
+            logger.Error("abcd");
+
+
+            var logger2 = LogManager.GetLogger("TimingLog");
+            logger2.Error("1234");
+
+            logger2.Error("abcd");
+            Console.ReadKey();
+        }
+    }
+}
