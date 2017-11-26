4f05e4b3a038bdf2f05b658345e2412d3f496789
forrestcai <caizi-wg@163.com>
2017-11-26 14:08:03 +0800

    t

---
+++ RabbitMQ/RabbitMQTest/src/Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var factory = new ConnectionFactory() { HostName = "192.168.0.131", Port = 5673, UserName = "guest", Password = "guest", Protocol = Protocols.AMQP_0_9_1 };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    while (!input.Equals("exit"))
+                    {
+                        //channel.ExchangeDeclare(exchange: "Test", type: "direct", durable: true);
+
+                        //channel.QueueDeclare(queue: "test",
+                        //         durable: false,
+                        //         exclusive: false,
+                        //         autoDelete: false,
+                        //         arguments: null);
+
+                        string message = input;
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        var p = channel.CreateBasicProperties();
+                        p.Persistent = true;
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "Test",
+                                             basicProperties: p,
+                                             body: body);
+
+                        Console.WriteLine(" [x] Sent {0}", message);
+
+                        input = Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ/RabbitMQTest/src/Subscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Subscriber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.0.131", Port = 5673, UserName = "guest", Password = "guest", Protocol = Protocols.AMQP_0_9_1 };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var queue = channel.QueueDeclare(queue: "Test",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 3, global: false);
+
+            //channel.QueueBind(queue: queue.QueueName,
+            //                  exchange: "",
+            //                  routingKey: "");                              );
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received {0}", message);
+                Thread.Sleep(5000);
+                Console.WriteLine("Finished {0}", message);
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: queue.QueueName,                                
+                                 noAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine("Started");
+        }
+    }
+}
