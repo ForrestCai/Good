073703a3346972e9b2311010c67a87432b58bc60
forrestcai <caizi-wg@163.com>
2018-11-04 22:33:34 +0800

    te

---
+++ Java/Practice/cameltest/src/main/java/App.java
@@ -0,0 +1,8 @@
+@package Java.Beans.
+
+public class App {
+    public static void main(String[] args)
+    {
+        java.beans.XMLDecoder xd = new java.beans.XMLDecoder(new BufferedInputStream(new FileInputStream(file)));
+    }
+}
