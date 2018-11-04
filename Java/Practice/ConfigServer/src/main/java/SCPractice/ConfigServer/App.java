b0b26241ddbca37996ec78a5b4dec34bc472db0c
forrestcai <caizi-wg@163.com>
2018-11-04 21:49:47 +0800

    sp

--- Java/Practice/ConfigServer/src/main/java/SCPractice/ConfigServer/App.java
+++ Java/Practice/ConfigServer/src/main/java/SCPractice/ConfigServer/App.java
@@ -1,20 +1,20 @@
 package SCPractice.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Hello world!
  *
  */
-@SpringBootConfiguration
-@EnableAutoConfiguration 
+@SpringBootApplication
+@EnableConfigServer
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        SpringApplication.run(SpringApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
